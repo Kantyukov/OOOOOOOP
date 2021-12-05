5ed1ddede255c3b627fdcf1557fc919815e70bbe
Kantyukov <tema_genius@mail.ru>
2021-12-05 20:54:02 +0300

    Lombok

--- src/main/java/ru/artem/kantukov/instruments/Instruments.java
+++ src/main/java/ru/artem/kantukov/instruments/Instruments.java
@@ -1,17 +1,20 @@
 package ru.artem.kantukov.instruments;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Instruments {
     private final String name;
     private final String melody;
     private final double price;
     private String size;
 
-    public Instruments (String name,String melody, double price, String size){
-        this.melody = melody;
-        this.name=name;
-        this.price = price;
-        this.size = size;
-    }
+//    public Instruments (String name,String melody, double price, String size){
+//        this.melody = melody;
+//        this.name=name;
+//        this.price = price;
+//        this.size = size;
+//    }
     public String toString (){
         return  " [ " + name+ " :  " +melody+ "  по цене: " +price+ "$ и имеет размер: " +size+ "]";
     }
