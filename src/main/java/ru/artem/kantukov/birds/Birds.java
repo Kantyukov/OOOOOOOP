041cf4e3bc8ab4db541ea7d590ef2221eb4bf570
Kantyukov <tema_genius@mail.ru>
2021-12-05 20:51:14 +0300

    Lombok

--- src/main/java/ru/artem/kantukov/birds/Birds.java
+++ src/main/java/ru/artem/kantukov/birds/Birds.java
@@ -1,20 +1,42 @@
 package ru.artem.kantukov.birds;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Birds {
 
-        private final String name;
-        private final String cry;
-        private boolean voice;
+    private final String name;
+    private final String cry;
+    private boolean voice;
 
-        public Birds(String name, String cry, boolean voice) {
-            this.name = name;
-            this.cry = cry;
-            this.voice =voice;
+   // public Birds(String name, String cry, boolean voice) {
+    //   this.name = name;
+    // this.cry = cry;
+    //    this.voice = voice;
 
-        }
-        public String toString(){
-            return  "{ " +name+ ": " +cry+ "} { voice: " +voice+ " }";
-        }
+    //}
+    @Override
+    public String toString() {
+        return "{ " + name + ": " + cry + "} { voice: " + voice + " }";
+    }
 
+    @Override //Переопределяем equals
+    public boolean equals(Object o) {
+        if (this == o) return true; // Если сылаемся на один объект возвращаемя тру
+        if (!(o instanceof Birds)) return false; // Мы проверяем является ли объект экземпляром класса Птица. Если нет, возвращает фалсе
+        Birds birds = (Birds) o; // Приводим тип данных
+
+        if (name != birds.name) return false; // Савниваем имя и именем объекта, если не равны выводим фалс
+        return cry.equals(birds.cry);   // Если все тру сравниваем другие параметры
+    }
+
+    @Override // Перепределяем HashCode
+    public int hashCode(){
+        return name.hashCode();
+    }
 
 }
--- src/main/java/ru/artem/kantukov/birds/Oop.java
+++ src/main/java/ru/artem/kantukov/birds/Oop.java
@@ -5,7 +5,12 @@ public class Oop {
         Birds kesha = new Parrots();
         System.out.println(kesha.toString());
         Birds vorona = new Crows();
+        Birds popugai = new Parrots();
         System.out.println(vorona.toString());
+        System.out.println("kesha.equals(vorona) = " + kesha.equals(vorona));
+        System.out.println("kesha.hashCode() = " + kesha.hashCode());
+        System.out.println("kesha.equals(kesha) = " + kesha.equals(kesha));
+        System.out.println("popugai.equals(kesha) = " + popugai.equals(kesha));
     }
 
 
