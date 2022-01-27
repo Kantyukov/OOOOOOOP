ed35c40ba2cda49b24369e9d175af0a768ae5529
Kantyukov Artem <Tema_genius@mail.ru>
2022-01-27 14:41:10 +0300

    remarked

--- src/main/java/ru/artem/kantukov/iioStreamPractice/IOStreamPractice.java
+++ src/main/java/ru/artem/kantukov/iioStreamPractice/IOStreamPractice.java
@@ -5,6 +5,8 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IOStreamPractice {
     public static void main(String[] args) throws IOException, FileAlreadyExistsException {
@@ -20,54 +22,54 @@ public class IOStreamPractice {
         } catch (FileNotFoundException fileNotFoundException) {
             fileNotFoundException.printStackTrace();
         }
+        List<File> fileWithText1 = new ArrayList<>();
+        List<File> fileTResText = new ArrayList<>();
+        List<File> fileWithText2 = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
-            String input = null;
-            while ((input = reader.readLine()) != null) {
-                System.out.println(input);
+            String input1 = null;
+            while ((input1 = reader.readLine()) != null) {
+                fileWithText1.add(new File(input1));
             }
+            System.out.println(fileWithText1);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-        Files.copy(Path.of("src/main/resources/file.txt"), Path.of("src/main/resources/file1.txt"), StandardCopyOption.REPLACE_EXISTING);
-        System.out.println();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file1));) {
-            String input = null;
-            while ((input = reader.readLine()) != null) {
-                System.out.println(input);
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
+//        Files.copy(Path.of("src/main/resources/file.txt"), Path.of("src/main/resources/file1.txt"), StandardCopyOption.REPLACE_EXISTING);
+        try (PrintWriter printWriterText = new PrintWriter(file2)) {
+            printWriterText.println("Но что в моей стране труда\n" +
+                    "Теперь произошло,\n" +
+                    "То лучезарным навсегда\n" +
+                    "В историю вошло");
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
         }
-
-       try(PrintWriter printWriterText = new PrintWriter(file2)){
-           printWriterText.println("Но что в моей стране труда\n" +
-                   "Теперь произошло,\n" +
-                   "То лучезарным навсегда\n" +
-                   "В историю вошло");
-       } catch (FileNotFoundException fileNotFoundException) {
-           fileNotFoundException.printStackTrace();
-       }
-        System.out.println();
         try (BufferedReader reader = new BufferedReader(new FileReader(file2));) {
             String input = null;
             while ((input = reader.readLine()) != null) {
-                System.out.println(input);
+                fileWithText2.add(new File(input));
             }
+            System.out.println(fileWithText2);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-        Files.copy(Path.of("src/main/resources/file2.txt"), Path.of("src/main/resources/file.txt"), StandardCopyOption.REPLACE_EXISTING);
-        Files.copy(Path.of("src/main/resources/file1.txt"), Path.of("src/main/resources/file2.txt"),StandardCopyOption.REPLACE_EXISTING);
+//        Files.copy(Path.of("src/main/resources/file2.txt"), Path.of("src/main/resources/file.txt"), StandardCopyOption.REPLACE_EXISTING);
+//        Files.copy(Path.of("src/main/resources/file1.txt"), Path.of("src/main/resources/file2.txt"),StandardCopyOption.REPLACE_EXISTING);
         System.out.println();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file));
-                BufferedReader reader1 = new BufferedReader(new FileReader(file2));) {
-            String input = null;
-            while ((input = reader.readLine()) != null) {
-                System.out.println(input);
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
+        fileTResText = fileWithText1;
+        fileWithText1 = fileWithText2;
+        fileWithText2 = fileTResText;
+        System.out.println(fileWithText1);
+        System.out.println(fileWithText2);
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            printWriter.println(fileWithText1);
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }try (PrintWriter printWriter = new PrintWriter(file2)) {
+            printWriter.println(fileWithText2);
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
         }
+
     }
 }
